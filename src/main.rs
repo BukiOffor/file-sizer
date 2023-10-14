b8a94224c90a740d40ed43737a66028217d46e37
Ebuka <ebuka2264@yahoo.com>
2023-10-15 00:40:48 +0100

    main: fix permission issues

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,95 @@
 #![allow(unused)]
+use std::io::ErrorKind;
 use std::process::Command;
 use std::ffi::OsString;
-use std::fs::File;
-use std::{fs,thread,path::Path,env};
+use std::fs::{File, Metadata};
+use std::{fs, fs::metadata, thread, path::Path, path::PathBuf, env, time};
 use std::env::{current_dir, set_current_dir};
+use std::mem::drop;
+
 
 
 //use std::path;
 fn main() {
     read_files_in_folder();
 }
+
 fn read_files_in_folder(){
 
+    let path = PathBuf::from("/Users/mac");
     let dir = current_dir().unwrap();
     for files in fs::read_dir(&dir).expect("{file:?} failed to open"){
-        let file = &files.unwrap().file_name();
-        let mut f = File::open(file).unwrap().metadata().expect("{file:?} failed to open");
-            match f.is_dir() {
+        let meta_data = files.as_ref().unwrap().metadata().unwrap();
+        let file = &files.as_ref().unwrap().path();  
+            match meta_data.is_dir() {
                 true => {
-                    let paths = [Path::new(&dir), Path::new(file)];
-                    let file_path = env::join_paths(paths.iter()).unwrap();
-                    println!("{file_path:?} is about to enter");
-                    recursive(&file_path)
-
+                    let path = Path::new(&dir).join(file);
+                    println!("{path:?} is about to enter");
+                    //let duration = time::Duration::from_millis(100);
+                    //thread::sleep(duration);
+                    test_recur(&path);
                 },
-                false => println!("it is not a directory")
+                false => {
+                    let result = run_command(&file).unwrap_or_else(|error|{
+                        match error.kind(){
+                            ErrorKind::PermissionDenied => {
+                                println!("permmision to check this file denied");
+                            }
+                            _ => ()
+                        }
+                    });
+                    
+                }
 
             }
-        run_command(&file);
     }
     
-
-    println!("{dir:?}")
 }
 
-fn recursive(args: &OsString){
+
+fn test_recur(args: &PathBuf){
     for files in fs::read_dir(&args).expect("{files:?} failed to open"){
-        let file = &files.unwrap().file_name();
-        let mut f = File::open(file).unwrap().metadata().expect("{file:?} failed to open");
-            match f.is_dir() {
-                true => {
-                    let paths = [Path::new(&file), Path::new(file)];
-                    let file_path = env::join_paths(paths.iter()).unwrap();
-                    println!("{file_path:?} is about to enter");
-                    recursive(&file_path);
-
-                },
-                false => println!("it is not a directory")
-
+        let meta_data = files.as_ref().unwrap().metadata().unwrap();
+        let file = &files.as_ref().unwrap().path();
+        match meta_data.is_dir(){
+            true =>{
+                let file = &files.unwrap().path();
+                println!("{file:?}");
+                test_recur(file)
+            },
+            false => {
+                run_command(file);
             }
+        }
     }
 }
 
 
+fn run_command(args:&PathBuf)->std::io::Result<()>{
 
-fn run_command(args:&OsString){
     let output = if cfg!(target_os = "windows") {
         Command::new("cmd")
             .args(["/C", "echo This is windows"])
-            .spawn()
+            //.spawn()
+            .output()
             .expect("failed to execute process")
-} else {
+    } else {
+    //thread::sleep(time::Duration::from_millis(100));
     Command::new("du")
         .arg("-hs")
         .arg(args)
-        .spawn()
-        .expect("failed to execute process")
+        //.spawn()
+        .output()?            
+        //.expect("failed to execute process")
     };
-}
+    println!("{output:?}");
+    if output.status.success() {
+        let s = String::from_utf8_lossy(&output.stdout);
+        println!("{}", s);
+    }
+    Ok(())
 
-//TO DO
-// 1. Reads all file in the system and displays the size
-//  implement flags;
-//      --f = a file part for a more condenced search
-//      --s = a threshhold minimum filesize for the program to display the file
+
+    
+
+}
