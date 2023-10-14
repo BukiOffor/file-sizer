74a256936f9be0725b90601e3eaf797ea4b35cf0
Ebuka <ebuka2264@yahoo.com>
2023-10-14 14:02:43 +0100

    main:recursive

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,69 @@
-use std::fs;
+#![allow(unused)]
 use std::process::Command;
+use std::ffi::OsString;
+use std::fs::File;
+use std::{fs,thread,path::Path,env};
+use std::env::{current_dir, set_current_dir};
+
+
 //use std::path;
 fn main() {
     read_files_in_folder();
-    run_command();
+}
+fn read_files_in_folder(){
+
+    let dir = current_dir().unwrap();
+    for files in fs::read_dir(&dir).expect("{file:?} failed to open"){
+        let file = &files.unwrap().file_name();
+        let mut f = File::open(file).unwrap().metadata().expect("{file:?} failed to open");
+            match f.is_dir() {
+                true => {
+                    let paths = [Path::new(&dir), Path::new(file)];
+                    let file_path = env::join_paths(paths.iter()).unwrap();
+                    println!("{file_path:?} is about to enter");
+                    recursive(&file_path)
+
+                },
+                false => println!("it is not a directory")
+
+            }
+        run_command(&file);
+    }
+    
+
+    println!("{dir:?}")
 }
 
-fn read_files_in_folder(){
-    for files in fs::read_dir("/").unwrap(){
-        let file = files.unwrap().file_name();
-        println!("{file:?}")
+fn recursive(args: &OsString){
+    for files in fs::read_dir(&args).expect("{files:?} failed to open"){
+        let file = &files.unwrap().file_name();
+        let mut f = File::open(file).unwrap().metadata().expect("{file:?} failed to open");
+            match f.is_dir() {
+                true => {
+                    let paths = [Path::new(&file), Path::new(file)];
+                    let file_path = env::join_paths(paths.iter()).unwrap();
+                    println!("{file_path:?} is about to enter");
+                    recursive(&file_path);
+
+                },
+                false => println!("it is not a directory")
+
+            }
     }
 }
-fn run_command(){
-    let _output = if cfg!(target_os = "windows") {
+
+
+
+fn run_command(args:&OsString){
+    let output = if cfg!(target_os = "windows") {
         Command::new("cmd")
             .args(["/C", "echo This is windows"])
             .spawn()
             .expect("failed to execute process")
 } else {
     Command::new("du")
-        .arg("-s")
-        .arg("-h")
+        .arg("-hs")
+        .arg(args)
         .spawn()
         .expect("failed to execute process")
     };
