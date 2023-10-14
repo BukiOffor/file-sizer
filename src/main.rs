2a5fa0953a4f8f15d5eefbca1d6a8e85d8f41ca3
Ebuka <ebuka2264@yahoo.com>
2023-10-14 04:37:45 +0100

    skeleton: drafted

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
+use std::fs;
+use std::process::Command;
+//use std::path;
 fn main() {
-    println!("Hello, world!");
+    read_files_in_folder();
+    run_command();
 }
+
+fn read_files_in_folder(){
+    for files in fs::read_dir("/").unwrap(){
+        let file = files.unwrap().file_name();
+        println!("{file:?}")
+    }
+}
+fn run_command(){
+    let _output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", "echo This is windows"])
+            .spawn()
+            .expect("failed to execute process")
+} else {
+    Command::new("du")
+        .arg("-s")
+        .arg("-h")
+        .spawn()
+        .expect("failed to execute process")
+    };
+}
+
 //TO DO
 // 1. Reads all file in the system and displays the size
 //  implement flags;
