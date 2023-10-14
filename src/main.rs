31bd314d18546092812e6afe02cd1af2603217e2
Ebuka <ebuka2264@yahoo.com>
2023-10-14 02:41:16 +0100

    structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
     println!("Hello, world!");
 }
+//TO DO
+// 1. Reads all file in the system and displays the size
+//  implement flags;
+//      --f = a file part for a more condenced search
+//      --s = a threshhold minimum filesize for the program to display the file
