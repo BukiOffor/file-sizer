f79f40175cebfd6f41e345e66f0e4d09ac8c2960
Ebuka <ebuka2264@yahoo.com>
2023-10-17 03:32:37 +0100

    readme: updated

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# file-sizer
-This project runs through a directory and checks a file size
+## Summary
+This code implements a Rust program that searches for files bigger than 100mb in a given directory or the whole file system if no filepath is given. It uses the `clap` and `std` libraries for command line argument parsing and file size calculation, respectively.
+
+## Example Usage
+```rust
+// Run the program with a specific directory path
+$ ./sizer -p /path/to/directory
+
+// Run the program without specifying a directory path (searches the whole file system)
+$ ./sizer
+
+// Run the program with a specific directory path and custom file size threshold
+$ ./sizer -p /path/to/directory -s 200
+```
+
+## Code Analysis
+### Inputs
+- `matches`: A `clap::ArgMatches` object that contains the parsed command line arguments.
+___
+### Flow
+1. Initialize the logger for logging purposes.
+2. Parse the command line arguments using `clap` to get the `matches` object.
+3. Get the value of the `path` argument from the `matches` object.
+4. If a `path` value is provided, convert it to a `PathBuf` object and assign it to `home_dir`.
+5. Get the value of the `size` argument from the `matches` object.
+6. If a `size` value is provided, assign it to `size` as an `Option<&String>`.
+7. If a `path` value is provided, call the `entry` function from the `sizer` library with `home_dir` and `size` as arguments.
+8. If no `path` value is provided, create a `PathBuf` object for the root directory ("/") and assign it to `root_dir`.
+9. Call the `entry` function from the `sizer` library with `root_dir` and `size` as arguments.
+___
+### Outputs
+None. The function performs file size calculations and logging based on the provided command line arguments.
+___
--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,36 @@ struct Cli {
 
 
 
+/// This is the main function of a Rust program that searches for files bigger than 100mb in a given directory or the whole file system if no filepath is given.
+///
+/// # Example Usage
+/// ```
+/// // Run the program with a specific directory path
+/// $ ./sizer -p /path/to/directory
+///
+/// // Run the program without specifying a directory path (searches the whole file system)
+/// $ ./sizer
+///
+/// // Run the program with a specific directory path and custom file size threshold
+/// $ ./sizer -p /path/to/directory -s 200
+/// ```
+///
+/// # Arguments
+/// - `matches`: A `clap::ArgMatches` object that contains the parsed command line arguments.
+///
+/// # Flow
+/// 1. Initialize the logger for logging purposes.
+/// 2. Parse the command line arguments using `clap` to get the `matches` object.
+/// 3. Get the value of the `path` argument from the `matches` object.
+/// 4. If a `path` value is provided, convert it to a `PathBuf` object and assign it to `home_dir`.
+/// 5. Get the value of the `size` argument from the `matches` object.
+/// 6. If a `size` value is provided, assign it to `size` as an `Option<&String>`.
+/// 7. If a `path` value is provided, call the `entry` function from the `sizer` library with `home_dir` and `size` as arguments.
+/// 8. If no `path` value is provided, create a `PathBuf` object for the root directory ("/") and assign it to `root_dir`.
+/// 9. Call the `entry` function from the `sizer` library with `root_dir` and `size` as arguments.
+///
+/// # Outputs
+/// None. The function performs file size calculations and logging based on the provided command line arguments.
 fn main(){
     env_logger::init();
     let matches = Command::new("Sizer")
@@ -44,7 +74,7 @@ fn main(){
             
         }
         None => {
-            let size: Option<&String> = matches.get_one::<String>("size");
+            let size: Option<&String> = matches.get_one::<String>("size"); // make sure its a valid number
             //gets the current directory
             let root_dir = PathBuf::from("/");
             entry(&root_dir, size);
@@ -65,7 +95,7 @@ fn main(){
 
 
 
-fn second_main(){
+fn old_main(){
     
     env_logger::init();
     let args:Vec<String> = env::args().collect();
